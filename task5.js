5e90dda48619a5594d8fea30505bea740bb02248
Rosepriya Franci <rosepriyafranci@gmail.com>
2026-07-24 15:42:41 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,20 @@
+// =========================================
+// Task 5: Hospital Patient System
+// Classify a Record
+// =========================================
+
+// Check whether a patient is a senior citizen.
+
+let patientAge = 65;
+
+if (patientAge >= 60) {
+    console.log("Patient is a Senior Citizen.");
+} else {
+    console.log("Patient is not a Senior Citizen.");
+}
+
+/*
+Expected Output:
+
+Patient is a Senior Citizen.
+*/
